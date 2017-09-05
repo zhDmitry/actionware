1a6bc5d4abce8c39432292f0bea406273802dc25
Dmitry Zherebko <kraken@live.ru>
2017-09-05 18:38:09 +0300

    up

--- src/createAction.js
+++ src/createAction.js
@@ -1,44 +1,44 @@
-import getActionName from './getActionName';
-import { Action } from './types';
-import { Store } from './types';
-import { TrackedAction } from './types';
-import { NAME_PREFIX } from './constants';
-import { ERROR_TYPE_SUFFIX } from './constants';
-import { BUSY_TYPE_SUFFIX } from './constants';
-import { getStore } from './storeKeeper';
-import { rejectWaiters } from './next';
-import { resolveWaiters } from './next';
-import { notifyErrorListeners } from './listeners';
-import { notifyBusyListeners } from './listeners';
-import { notifySuccessListeners } from './listeners';
+import getActionName from "./getActionName";
+import { Action } from "./types";
+import { Store } from "./types";
+import { TrackedAction } from "./types";
+import { NAME_PREFIX } from "./constants";
+import { ERROR_TYPE_SUFFIX } from "./constants";
+import { BUSY_TYPE_SUFFIX } from "./constants";
+import { getStore } from "./storeKeeper";
+import { rejectWaiters } from "./next";
+import { resolveWaiters } from "./next";
+import { notifyErrorListeners } from "./listeners";
+import { notifyBusyListeners } from "./listeners";
+import { notifySuccessListeners } from "./listeners";
 
 export default function createAction(
   action: Action,
   { prefix, name: customActionName } = {}
 ): TrackedAction {
-  if (typeof action !== 'function')
-    throw new Error('Action must be a function');
+  if (typeof action !== "function")
+    throw new Error("Action must be a function");
 
-  if (action._trackedAction) return action;
+  if (action._successType) return action;
 
   const trackedAction = function(...args) {
     const store: Store = getStore();
-    const handleError = handleActionError.bind(null, action, args);
+    const handleError = handleActionError.bind(null, trackedAction, args);
 
     try {
       const actionResponse = action.apply(null, [...args, store]);
       const isAsync = actionResponse instanceof Promise;
 
       if (isAsync) {
-        handleActionBusy(action, args);
+        handleActionBusy(trackedAction, args);
 
         return actionResponse.then(
-          payload => handleActionSuccess(action, payload),
+          payload => handleActionSuccess(trackedAction, payload),
           err => Promise.reject(handleError(err))
         );
       }
 
-      return handleActionSuccess(action, actionResponse);
+      return handleActionSuccess(trackedAction, actionResponse);
     } catch (error) {
       handleError(error);
       throw error;
@@ -47,28 +47,26 @@ export default function createAction(
 
   const actionName = getActionName(
     // if prefix exist add it else leave prefix empty if custom name provided
-    prefix ? prefix : customActionName ? '' : NAME_PREFIX,
+    prefix ? prefix : customActionName ? "" : NAME_PREFIX,
     customActionName || action.name,
     action
   );
 
-  console.log(actionName, customActionName, prefix, 'name', action.name);
   trackedAction._successType = actionName;
   trackedAction._busyType = actionName + BUSY_TYPE_SUFFIX;
   trackedAction._errorType = actionName + ERROR_TYPE_SUFFIX;
-
-  action._trackedAction = trackedAction;
-  console.log('action', action);
-
-  return action;
+  trackedAction.actionCreator = (...args) => ({
+    ...action.apply(null, args),
+    type: trackedAction._successType
+  });
+  return trackedAction;
 }
 
 export function handleActionSuccess(action: Action, payload) {
   const store: Store = getStore();
-
   store.dispatch({
-    trackedAction: action._trackedAction,
-    type: action._trackedAction._successType,
+    trackedAction: action,
+    type: action._successType,
     payload
   });
   notifySuccessListeners(action, payload, [store]);
@@ -80,9 +78,10 @@ export function handleActionSuccess(action: Action, payload) {
 
 export function handleActionBusy(action: Action, args) {
   const store: Store = getStore();
+
   store.dispatch({
-    trackedAction: action._trackedAction,
-    type: action._trackedAction._busyType,
+    trackedAction: action,
+    type: action._busyType,
     payload: true,
     args
   });
@@ -96,8 +95,8 @@ export function handleActionError(action: Action, args, error: Error) {
   const payload = { error, args, actionwareError: true };
 
   store.dispatch({
-    trackedAction: action._trackedAction,
-    type: action._trackedAction._errorType,
+    trackedAction: action,
+    type: action._errorType,
     payload
   });
 
@@ -108,13 +107,13 @@ export function handleActionError(action: Action, args, error: Error) {
 }
 
 export function errorType(action: Action) {
-  return action._trackedAction._errorType;
+  return action._errorType;
 }
 
 export function successType(action: Action) {
-  return action._trackedAction._successType;
+  return action._successType;
 }
 
 export function busyType(action: Action) {
-  return action._trackedAction._busyType;
+  return action._busyType;
 }
